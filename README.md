dec1bf7c4e4872c0d8881a98f2d7a263b38c7b4a
stoically <stoically@protonmail.com>
2019-06-23 13:39:39 +0200

    Add omnixbox switching capabilities

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Switch Container Plus
 
-The Add-on and informations about it can be found on [AMO](https://addons.mozilla.org/firefox/addon/switch-container-plus/).
+The Add-on and information about it can be found on [AMO](https://addons.mozilla.org/firefox/addon/switch-container-plus/).
 
-## Fork
+## Credits / Forks
 
-This is a fork of [Switch Container](https://gitlab.com/mjanetmars/switch-container) because the author denied a [Merge request](https://gitlab.com/mjanetmars/switch-container/merge_requests/2) introducing middle mouse clicks. Thanks to MarsCat for the main work on this Add-on. Everything is [contributed back upstream](https://gitlab.com/mjanetmars/switch-container/merge_requests?scope=all&utf8=%E2%9C%93&state=all&author_username=stoically).
+This is a fork of [Switch Container](https://gitlab.com/mjanetmars/switch-container) and [Search and Switch Containers](https://github.com/andreicristianpetcu/Search-and-Switch-Containers).
 
 ## License
 
-Mozilla Public License Version 2.0
+GPL-3.0
--- background.js
+++ background.js
@@ -1,4 +1,7 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/*
+  Forked from: https://gitlab.com/mjanetmars/switch-container
+
+  This Source Code Form is subject to the terms of the Mozilla Public
  - License, v. 2.0. If a copy of the MPL was not distributed with this
  - file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
@@ -37,5 +40,60 @@ var currentTabs = browser.tabs.query({});
 currentTabs.then((tabs) => {
   for (let tab of tabs) {
     initializePageAction( tab );
+  }
+});
+
+
+
+/*
+  Forked from https://github.com/andreicristianpetcu/Search-and-Switch-Containers
+
+  License: GPL-3.0
+*/
+browser.omnibox.setDefaultSuggestion({
+  description: `Search for containers and switch to them (e.g. "co personal" or "co banking")`
+});
+
+browser.omnibox.onInputChanged.addListener(async (text, addSuggestions) => {
+  const contexts = await browser.contextualIdentities.query({});
+  const result = [];
+  contexts.push({
+    name: "default",
+    description: `Switch to container: default`
+  });
+  for (let context of contexts) {
+    if (context.name.toLowerCase().indexOf(text.toLowerCase()) > -1) {
+      result.push({
+        content: context.name,
+        description: `Switch to container: ${context.name}`
+      })
+    }
+  }
+  addSuggestions(result);
+});
+
+browser.omnibox.onInputEntered.addListener(async (text, disposition) => {
+  const contexts = await browser.contextualIdentities.query({});
+  const tabs = await browser.tabs.query({ currentWindow: true, active: true });
+  contexts.push({
+    cookieStoreId: 'firefox-default',
+    name: 'default'
+  });
+
+  for (let context of contexts) {
+    if (context.name.toLowerCase().indexOf(text.toLowerCase()) > -1) {
+      let tabCreateProperties = {
+        cookieStoreId: context.cookieStoreId,
+        index: tabs[0].index
+      };
+      if(tabs[0].url !== 'about:newtab' &&
+         tabs[0].url !== 'about:blank' &&
+         tabs[0].url !== 'about:home') {
+        tabCreateProperties.url = tabs[0].url;
+      }
+      await browser.tabs.create(tabCreateProperties);
+      browser.tabs.remove(tabs[0].id);
+      break
+    }
   }
 });
